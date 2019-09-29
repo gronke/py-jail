ab72d7130eb66bab230680ce8b2955bed1246073
Stefan Grönke <stefan@gronke.net>
2019-09-29 21:24:24 +0000

    reference the dependency on freebsd_sysctl in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,26 @@ import os
 import os.path
 import sys
 from setuptools import find_packages, setup
+try:
+    from pip._internal.req import parse_requirements
+except ModuleNotFoundError:
+    from pip.req import parse_requirements
+
+
+def _read_requirements(
+    filename: str="requirements.txt"
+) -> typing.Dict[str, typing.List[str]]:
+    reqs = list(parse_requirements(filename, session="ioc_cli"))
+    return dict(
+        install_requires=list(map(lambda x: f"{x.name}{x.specifier}", reqs)),
+        dependency_links=list(map(
+            lambda x: str(x.link),
+            filter(lambda x: x.link, reqs)
+        ))
+    )
+
+
+requirements = _read_requirements("requirements.txt")
 
 cwd = os.getcwd()
 
@@ -24,7 +44,8 @@ setup(
 	author="Stefan Grönke",
 	author_email="stefan@gronke.net",
 	python_requires=">=3.6",
-	setup_requires=["pytest-runner", "pytest-benchmark"],
-	tests_require=["pytest"],
+    install_requires=requirements["install_requires"],
+    dependency_links=requirements["dependency_links"],
+	tests_require=["pytest", "pytest-runner", "pytest-benchmark"],
 	packages=find_packages(exclude=("tests",))
 )
