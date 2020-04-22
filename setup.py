f50826a54c844e8c3b2d58b4bf885268f15b0878
Igor Galić <me+git@igalic.co>
2020-04-22 11:34:12 +0200

    add cirrus.yml & Makefile & requirements-dev.txt from libioc
    and start fixing issues

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import typing
 import os
 import os.path
-import sys
 from setuptools import find_packages, setup
 try:
     from pip._internal.req import parse_requirements
@@ -34,23 +33,23 @@ cwd = os.getcwd()
 
 about = {}
 with open(os.path.join(cwd, "jail", "__version__.py"), encoding="utf-8") as f:
-    VERSION = exec(f.read(), about)
+    VERSION = exec(f.read(), about)  # nosec
 
 with open(os.path.join(cwd, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-	name="jail",
-	version=about["__version__"],
-	description="Native FreeBSD jail bindings with libc.",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/gronke/py-jail",
-	author="Stefan Grönke",
-	author_email="stefan@gronke.net",
-	python_requires=">=3.6",
+    name="jail",
+    version=about["__version__"],
+    description="Native FreeBSD jail bindings with libc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gronke/py-jail",
+    author="Stefan Grönke",
+    author_email="stefan@gronke.net",
+    python_requires=">=3.6",
     install_requires=requirements["install_requires"],
     dependency_links=requirements["dependency_links"],
-	tests_require=["pytest", "pytest-runner", "pytest-benchmark"],
-	packages=find_packages(exclude=("tests",))
+    tests_require=["pytest", "pytest-runner", "pytest-benchmark"],
+    packages=find_packages(exclude=("tests",))
 )
