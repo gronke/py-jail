e8442fdc9099f9c0390d3be016e8439ec800405f
Stefan Grönke <stefan@gronke.net>
2021-06-24 08:28:08 +0200

    bump version to 0.0.14

--- jail/__version__.py
+++ jail/__version__.py
@@ -22,6 +22,6 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """Version of the jail module."""
-VERSION = (0, 0, 13)
+VERSION = (0, 0, 14)
 
 __version__ = '.'.join(map(str, VERSION))
